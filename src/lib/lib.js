b45c718ce50f6d4d8dda3e8522e9c92983376ddc
Haxxer <adamoresten@gmail.com>
2023-10-14 12:56:35 +0100

    Added file states

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,50 +1,50 @@
 import CONSTANTS from "../constants.js";
 
 export function isActiveGM(user) {
-  return user.active && user.isGM;
+	return user.active && user.isGM;
 }
 
 export function getActiveGMs() {
-  return game.users.filter(isActiveGM);
+	return game.users.filter(isActiveGM);
 }
 
 export function getActiveUsers() {
-  return game.users.filter(user => user.active);
+	return game.users.filter(user => user.active);
 }
 
 export function isResponsibleGM() {
-  if (!game.user.isGM) {
-    return false;
-  }
-  return !getResponsibleGM();
+	if (!game.user.isGM) {
+		return false;
+	}
+	return !getResponsibleGM();
 }
 
 export function getResponsibleGM() {
-  return getActiveGMs().find(other => other.id < game.user.id);
+	return getActiveGMs().find(other => other.id < game.user.id);
 }
 
 export function isGMConnected() {
-  return !!Array.from(game.users).find(user => user.isGM && user.active);
+	return !!Array.from(game.users).find(user => user.isGM && user.active);
 }
 
 export function getSceneDelegator() {
 
-  const activeUsers = getActiveUsers().filter(user => {
-    return user.viewedScene === game.user.viewedScene
-  });
+	const activeUsers = getActiveUsers().filter(user => {
+		return user.viewedScene === game.user.viewedScene
+	});
 
-  activeUsers.sort((a, b) => {
-    return (getProperty(b, CONSTANTS.UPDATED_FLAG) ?? 0) - (getProperty(a, CONSTANTS.UPDATED_FLAG) ?? 0);
-  });
+	activeUsers.sort((a, b) => {
+		return (getProperty(b, CONSTANTS.UPDATED_FLAG) ?? 0) - (getProperty(a, CONSTANTS.UPDATED_FLAG) ?? 0);
+	});
 
-  activeUsers.sort((a, b) => b.isGM - a.isGM);
+	activeUsers.sort((a, b) => b.isGM - a.isGM);
 
-  return activeUsers[0];
+	return activeUsers[0];
 }
 
 export function isRealNumber(n) {
-  const num = Number(n);
-  return typeof num == 'number' && !isNaN(num) && isFinite(num);
+	const num = Number(n);
+	return typeof num == 'number' && !isNaN(num) && isFinite(num);
 }
 
 /**
@@ -55,9 +55,9 @@ export function isRealNumber(n) {
  * @return {number}                             A random value between the range given
  */
 export function randomFloatBetween(min, max) {
-  const _max = Math.max(max, min);
-  const _min = Math.min(max, min);
-  return Math.random() * (_max - _min) + _min;
+	const _max = Math.max(max, min);
+	const _min = Math.min(max, min);
+	return Math.random() * (_max - _min) + _min;
 }
 
 /**
@@ -68,142 +68,151 @@ export function randomFloatBetween(min, max) {
  * @return {int}                                A random integer between the range given
  */
 export function randomIntegerBetween(min, max) {
-  return Math.floor(randomFloatBetween(min, max));
+	return Math.floor(randomFloatBetween(min, max));
 }
 
 export function transformNumber(num) {
-  // Flip the input number
-  const flippedNum = 1 - num;
+	// Flip the input number
+	const flippedNum = 1 - num;
 
-  // Apply exponential transformation with base 2
-  const transformedNum = 1 - Math.pow(2, -flippedNum);
+	// Apply exponential transformation with base 2
+	const transformedNum = 1 - Math.pow(2, -flippedNum);
 
-  // Flip the output number
-  return 1 - transformedNum;
+	// Flip the output number
+	return 1 - transformedNum;
 }
 
 export async function getWildCardFiles(inFile) {
 
-  if (!inFile) return false;
+	if (!inFile) return false;
 
-  let source = 'data';
-  const browseOptions = { wildcard: true };
+	let source = 'data';
+	const browseOptions = { wildcard: true };
 
-  if (/\.s3\./.test(inFile)) {
-    source = 's3'
-    const { bucket, keyPrefix } = FilePicker.parseS3URL(inFile);
-    if (bucket) {
-      browseOptions.bucket = bucket;
-      inFile = keyPrefix;
-    }
-  }
+	if (/\.s3\./.test(inFile)) {
+		source = 's3'
+		const { bucket, keyPrefix } = FilePicker.parseS3URL(inFile);
+		if (bucket) {
+			browseOptions.bucket = bucket;
+			inFile = keyPrefix;
+		}
+	}
 
-  try {
-    return (await FilePicker.browse(source, inFile, browseOptions)).files;
-  } catch (err) {
-    return false;
-  }
+	try {
+		return (await FilePicker.browse(source, inFile, browseOptions)).files;
+	} catch (err) {
+		return false;
+	}
 }
 
 export function getVideoDuration(src) {
-  return new Promise((resolve) => {
-    const video = document.createElement('video');
-    video.preload = 'metadata';
-    video.onloadedmetadata = function () {
-      resolve(video.duration);
-    }
-    video.src = src;
-  });
+	return new Promise((resolve) => {
+		const video = document.createElement('video');
+		video.preload = 'metadata';
+		video.onloadedmetadata = function () {
+			resolve(video.duration);
+		}
+		video.src = src;
+	});
 }
 
 export function validateStates(states) {
 
-  const errors = [];
+	const errors = [];
 
-  for (const [index, state] of states.entries()) {
+	for (const [index, state] of states.entries()) {
 
-    if (!(isRealNumber(state.start) || Object.values(CONSTANTS.START).some(val => val === state.start))) {
-      errors.push(`State "${state.name}" has an invalid value in its "start" setting`);
-    }
+		if (!(isRealNumber(state.start) || Object.values(CONSTANTS.START).some(val => val === state.start))) {
+			errors.push(`State "${state.name}" has an invalid value in its "start" setting`);
+		}
 
-    if (!(isRealNumber(state.end) || Object.values(CONSTANTS.END).some(val => val === state.end))) {
-      if (state.behavior === CONSTANTS.BEHAVIORS.STILL || state.behavior === CONSTANTS.BEHAVIORS.STILL_HIDDEN) {
-        state.end = "";
-      } else {
-        errors.push(`State "${state.name}" has an invalid value in its "end" setting`)
-      }
-    }
+		if (!(isRealNumber(state.end) || Object.values(CONSTANTS.END).some(val => val === state.end))) {
+			if (state.behavior === CONSTANTS.BEHAVIORS.STILL || state.behavior === CONSTANTS.BEHAVIORS.STILL_HIDDEN) {
+				state.end = "";
+			} else {
+				errors.push(`State "${state.name}" has an invalid value in its "end" setting`)
+			}
+		}
 
-    switch (state.behavior) {
-      case CONSTANTS.BEHAVIORS.ONCE_PREVIOUS:
-        if (index === 0) {
-          errors.push(`State "${state.name}" cannot have "once, previous" behavior because it is the first state`)
-        }
-        break;
-      case CONSTANTS.BEHAVIORS.ONCE_NEXT:
-        if (index === states.length - 1) {
-          errors.push(`State "${state.name}" cannot have "once, next" behavior because it is the last state`)
-        }
-        break;
-    }
+		switch (state.behavior) {
+			case CONSTANTS.BEHAVIORS.ONCE_PREVIOUS:
+				if (index === 0) {
+					errors.push(`State "${state.name}" cannot have "once, previous" behavior because it is the first state`)
+				}
+				break;
+			case CONSTANTS.BEHAVIORS.ONCE_NEXT:
+				if (index === states.length - 1) {
+					errors.push(`State "${state.name}" cannot have "once, next" behavior because it is the last state`)
+				}
+				break;
+		}
 
-    if (state.start === CONSTANTS.START.PREV) {
-      const previousState = states?.[index - 1];
-      if (previousState) {
-        if (!isRealNumber(previousState.end)) {
-          errors.push(`State "${state.name}" cannot have "prev" as its start time, because state "${previousState.name}" does not end at a specific time`)
-        }
-      } else {
-        errors.push(`State "${state.name}" cannot have "prev" as its start time, because it is the first state`)
-      }
-    }
+		if (state.start === CONSTANTS.START.PREV) {
+			const previousState = states?.[index - 1];
+			if (previousState) {
+				if (!isRealNumber(previousState.end)) {
+					errors.push(`State "${state.name}" cannot have "prev" as its start time, because state "${previousState.name}" does not end at a specific time`)
+				}
+			} else {
+				errors.push(`State "${state.name}" cannot have "prev" as its start time, because it is the first state`)
+			}
+		}
 
-    if (state.end === CONSTANTS.END.NEXT) {
-      const nextState = states?.[index + 1];
-      if (nextState) {
-        if (!isRealNumber(nextState.start)) {
-          errors.push(`State "${state.name}" cannot have "next" as its end time, because state "${nextState.name}" does not start at a specific time`)
-        }
-      } else {
-        errors.push(`State "${state.name}" cannot have "next" as its end time, because it is the last state`)
-      }
-    }
-  }
-  return errors;
+		if (state.end === CONSTANTS.END.NEXT) {
+			const nextState = states?.[index + 1];
+			if (nextState) {
+				if (!isRealNumber(nextState.start)) {
+					errors.push(`State "${state.name}" cannot have "next" as its end time, because state "${nextState.name}" does not start at a specific time`)
+				}
+			} else {
+				errors.push(`State "${state.name}" cannot have "next" as its end time, because it is the last state`)
+			}
+		}
+	}
+	return errors;
 }
 
 export function determineFileColor(inFile) {
 
-  const lowerCaseFile = decodeURI(inFile.toLowerCase());
+	const lowerCaseFile = decodeURI(inFile.toLowerCase());
 
-  for (const [colorName, color] of Object.entries(CONSTANTS.COLOR_CODE)) {
-    if (lowerCaseFile.endsWith(`__${colorName}.webm`)) {
-      return { colorName, color, tooltip: CONSTANTS.COLOR_NAME[colorName] };
-    }
-  }
+	for (const [colorName, color] of Object.entries(CONSTANTS.COLOR_CODE)) {
+		if (lowerCaseFile.endsWith(`__${colorName}.webm`)) {
+			return { colorName, color, tooltip: CONSTANTS.COLOR_NAME[colorName] };
+		}
+	}
 
-  return {
-    colorName: false,
-    color: CONSTANTS.COLOR_CODE["none"],
-    tooltip: CONSTANTS.COLOR_NAME["none"]
-  };
+	return {
+		colorName: false,
+		color: CONSTANTS.COLOR_CODE["none"],
+		tooltip: CONSTANTS.COLOR_NAME["none"]
+	};
 
 }
 
 export function getThumbnailVariations(url) {
-  return Object.keys(CONST.IMAGE_FILE_EXTENSIONS).map(ext => url.replace(".webm", "." + ext));
+	return Object.keys(CONST.IMAGE_FILE_EXTENSIONS).map(ext => url.replace(".webm", "." + ext));
 }
 
 export function getVideoJsonPath(placeableDocument) {
-  return decodeURI(placeableDocument.texture.src).split("  ")[0]
-    .replace(".webm", "") + ".json";
+	return decodeURI(placeableDocument.texture.src).split("  ")[0]
+		.replace(".webm", "") + ".json";
 }
 
 export function createJsonFile(placeableDocument, inData) {
-  const path = getVideoJsonPath(placeableDocument)
-  const splitPath = path.split('/');
-  const serializedData = JSON.stringify(inData);
-  const blob = new Blob([serializedData], { type: 'application/json' });
-  const file = new File([blob], splitPath.pop());
-  return FilePicker.upload("data", splitPath.join('/'), file, {}, { notify: false });
+	const path = getVideoJsonPath(placeableDocument)
+	const splitPath = path.split('/');
+	const serializedData = JSON.stringify(inData, null, 4);
+	const blob = new Blob([serializedData], { type: 'application/json' });
+	const file = new File([blob], splitPath.pop());
+	return FilePicker.upload("data", splitPath.join('/'), file, {}, { notify: false });
+}
+
+
+export function getFolder(path) {
+	const folderParts = path.split("/");
+	if (folderParts.length > 1) {
+		folderParts.pop();
+	}
+	return folderParts.join("/")
 }
--- src/module.js
+++ src/module.js
@@ -5,143 +5,160 @@ import CONSTANTS from "./constants.js";
 import { copiedData, StatefulVideo } from "./StatefulVideo.js";
 import SocketHandler from "./socket.js";
 import * as lib from "./lib/lib.js";
+import { getFolder } from "./lib/lib.js";
 
 Hooks.once('init', async function () {
-  registerLibwrappers();
-  SocketHandler.initialize();
-  StatefulVideo.registerHooks();
+	registerLibwrappers();
+	SocketHandler.initialize();
+	StatefulVideo.registerHooks();
 
-  game.thekinemancer = {
-    updateState: (uuid, options) => StatefulVideo.changeVideoState(uuid, options),
-    StatefulVideo,
-    CONSTANTS,
-    copiedData,
-    lib
-  };
+	game.thekinemancer = {
+		updateState: (uuid, options) => StatefulVideo.changeVideoState(uuid, options),
+		StatefulVideo,
+		CONSTANTS,
+		copiedData,
+		lib
+	};
 });
 
 Hooks.once('ready', async function () {
 
-  setTimeout(() => {
-    StatefulVideo.determineCurrentDelegator();
-  }, 250);
+	setTimeout(() => {
+		StatefulVideo.determineCurrentDelegator();
+	}, 250);
 
-  document.addEventListener("visibilitychange", function () {
-    if (document.hidden) {
-      StatefulVideo.getAll().forEach(statefulVideo => {
-        statefulVideo.video.pause();
-      });
-    } else {
-      StatefulVideo.getAll().forEach(statefulVideo => {
-        statefulVideo.offset = Number(Date.now()) - statefulVideo.flags.updated;
-        game.video.play(statefulVideo.video);
-      });
-    }
-  });
+	document.addEventListener("visibilitychange", function () {
+		if (document.hidden) {
+			StatefulVideo.getAll().forEach(statefulVideo => {
+				statefulVideo.video.pause();
+			});
+		} else {
+			StatefulVideo.getAll().forEach(statefulVideo => {
+				statefulVideo.offset = Number(Date.now()) - statefulVideo.flags.updated;
+				game.video.play(statefulVideo.video);
+			});
+		}
+	});
+
+	if (game.user.isGM) {
+		for (const scene of Array.from(game.scenes)) {
+			const updates = Array.from(scene.tiles).map(tile => {
+				if (!getProperty(tile, CONSTANTS.STATES_FLAG)?.length || getProperty(tile, CONSTANTS.BASE_FILE_FLAG)) return false;
+				return {
+					_id: tile.id,
+					[CONSTANTS.BASE_FILE_FLAG]: tile?.texture?.src,
+					[CONSTANTS.FOLDER_PATH_FLAG]: lib.getFolder(tile?.texture?.src),
+				}
+			}).filter(Boolean);
+			if (updates.length) {
+				await scene.updateEmbeddedDocuments("Tile", updates);
+			}
+		}
+	}
 
 });
 
 
 Hooks.on('renderFilePicker', (filePicker, html) => {
 
-  const regex = new RegExp(/^.*?the-kinemancer\/.+__(.+).webm$/, "g")
+	const regex = new RegExp(/^.*?the-kinemancer\/.+__(.+).webm$/, "g")
 
-  html.find('[data-src="icons/svg/video.svg"]:visible').each((idx, imgElem) => {
+	html.find('[data-src="icons/svg/video.svg"]:visible').each((idx, imgElem) => {
 
-    const img = $(imgElem);
-    const parent = img.closest('[data-path]');
-    const path = parent.data('path');
-    const width = img.attr('width');
-    const height = img.attr('height');
+		const img = $(imgElem);
+		const parent = img.closest('[data-path]');
+		const path = parent.data('path');
+		const width = img.attr('width');
+		const height = img.attr('height');
 
-    if (path.match(regex)) {
-      parent.remove();
-      return;
-    }
+		if (path.match(regex)) {
+			parent.remove();
+			return;
+		}
 
-    new Promise(async (resolve) => {
-      let found = false;
-      const splitPath = path.split("/");
-      const file_name = splitPath.pop();
-      const variationPath = splitPath.join("/") + "/stills/" + file_name.replace(".webm", ".webp");
-      try {
-        await FilePicker.browse("data", variationPath).then(() => {
-          found = true;
-          setTimeout(() => {
-            img.attr("src", variationPath);
-          }, 150);
-        })
-      } catch (err) {
-      }
-      resolve();
-    });
+		new Promise(async (resolve) => {
+			let found = false;
+			const splitPath = path.split("/");
+			const file_name = splitPath.pop();
+			const variationPath = splitPath.join("/") + "/stills/" + file_name.replace(".webm", ".webp");
+			try {
+				await FilePicker.browse("data", variationPath).then(() => {
+					found = true;
+					setTimeout(() => {
+						img.attr("src", variationPath);
+					}, 150);
+				})
+			} catch (err) {
+			}
+			resolve();
+		});
 
-    const video = $(`<video class="fas video-preview" loop width="${width}" height="${height}"></video>`);
-    video.hide();
-    parent.append(video);
-    const videoElem = video.get(0);
-    let playTimeout = null;
+		const video = $(`<video class="fas video-preview" loop width="${width}" height="${height}"></video>`);
+		video.hide();
+		parent.append(video);
+		const videoElem = video.get(0);
+		let playTimeout = null;
 
-    parent.addClass('video-parent');
+		parent.addClass('video-parent');
 
-    parent.on("mouseenter", () => {
-      if (!videoElem.src) {
-        parent.addClass(' -loading');
-        videoElem.addEventListener('loadeddata', () => {
-          parent.removeClass('-loading');
-        }, false);
-        videoElem.src = path;
-      }
-      img.hide();
-      video.show();
-      playTimeout = setTimeout(() => {
-        videoElem.currentTime = 0;
-        videoElem.play().catch(e => console.error(e));
-      }, !!videoElem.src ? 0 : 750);
-    }).on("mouseleave", () => {
-      clearTimeout(playTimeout);
-      videoElem.pause();
-      videoElem.currentTime = 0;
-      video.hide();
-      img.show();
-    });
-  });
+		parent.on("mouseenter", () => {
+			if (!videoElem.src) {
+				parent.addClass(' -loading');
+				videoElem.addEventListener('loadeddata', () => {
+					parent.removeClass('-loading');
+				}, false);
+				videoElem.src = path;
+			}
+			img.hide();
+			video.show();
+			playTimeout = setTimeout(() => {
+				videoElem.currentTime = 0;
+				videoElem.play().catch(e => console.error(e));
+			}, !!videoElem.src ? 0 : 750);
+		}).on("mouseleave", () => {
+			clearTimeout(playTimeout);
+			videoElem.pause();
+			videoElem.currentTime = 0;
+			video.hide();
+			img.show();
+		});
+	});
 });
 
 
 function registerLibwrappers() {
 
-  libWrapper.register(CONSTANTS.MODULE_NAME, 'Tile.prototype._destroy', function (wrapped) {
-    if (this.isVideo) {
-      StatefulVideo.tearDown(this.document.uuid);
-    }
-    return wrapped();
-  }, "MIXED");
+	libWrapper.register(CONSTANTS.MODULE_NAME, 'Tile.prototype._destroy', function (wrapped) {
+		if (this.isVideo) {
+			StatefulVideo.tearDown(this.document.uuid);
+		}
+		return wrapped();
+	}, "MIXED");
 
-  libWrapper.register(CONSTANTS.MODULE_NAME, 'Token.prototype._destroy', function (wrapped) {
-    if (this.isVideo) {
-      StatefulVideo.tearDown(this.document.uuid);
-    }
-    return wrapped();
-  }, "MIXED");
+	libWrapper.register(CONSTANTS.MODULE_NAME, 'Token.prototype._destroy', function (wrapped) {
+		if (this.isVideo) {
+			StatefulVideo.tearDown(this.document.uuid);
+		}
+		return wrapped();
+	}, "MIXED");
 
-  libWrapper.register(CONSTANTS.MODULE_NAME, 'VideoHelper.prototype.play', async function (wrapped, video, options) {
-    for (const statefulVideo of StatefulVideo.getAll().values()) {
-      if (video === statefulVideo.video) {
-        if (this.locked || statefulVideo.destroyed || statefulVideo.playing || statefulVideo.still) return;
-        if (window.document.hidden) return video.pause();
-        const newOptions = statefulVideo.getVideoPlaybackState();
-        if (!newOptions) return;
-        return wrapped(video, newOptions);
-      }
-    }
-    return wrapped(video, options);
-  }, "MIXED");
+	libWrapper.register(CONSTANTS.MODULE_NAME, 'VideoHelper.prototype.play', async function (wrapped, video, options) {
+		for (const statefulVideo of StatefulVideo.getAll().values()) {
+			if (video === statefulVideo.video) {
+				if (this.locked || statefulVideo.destroyed || statefulVideo.playing || statefulVideo.still) return;
+				if (window.document.hidden) return video.pause();
+				const newOptions = statefulVideo.getVideoPlaybackState();
+				if (!newOptions) return;
+				return wrapped(video, newOptions);
+			}
+		}
+		return wrapped(video, options);
+	}, "MIXED");
 
-  libWrapper.register(CONSTANTS.MODULE_NAME, 'VideoHelper.prototype._onFirstGesture', async function (wrapped, event) {
-    Hooks.callAll("canvasFirstUserGesture");
-    return wrapped(event);
-  }, "MIXED");
+	libWrapper.register(CONSTANTS.MODULE_NAME, 'VideoHelper.prototype._onFirstGesture', async function (wrapped, event) {
+		Hooks.callAll("canvasFirstUserGesture");
+		return wrapped(event);
+	}, "MIXED");
 
 }
 
